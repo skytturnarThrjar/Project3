24917659fa56906db239466d45ab3351e2236e91
Laufey Rut Guðmundsdóttir <Laufey@Laufeys-MacBook-Pro.local>
2016-03-01 12:56:38 +0000

    Sellers

--- src/components/sellers/SellersController.js
+++ src/components/sellers/SellersController.js
@@ -4,5 +4,10 @@ angular.module("project3App").controller("SellersController",
 function SellersController($scope, AppResource) {
 	// TODO: load data from AppResource! Also, add other methods, such as to
 	// add/update sellers etc.
+	console.log(AppResource);
+	AppResource.getSellers().success(function (sellers ){
+		$scope.sellers = sellers;
+		console.log($scope.sellers);
 
+	});
 });
