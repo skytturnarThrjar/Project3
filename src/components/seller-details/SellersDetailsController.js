1af895e2cc26d2e7a526f35468d0d8d419499dd0
Laufey Rut Guðmundsdóttir <Laufey@Laufeys-MacBook-Pro.local>
2016-03-12 23:27:08 +0000

    bæ

--- src/components/seller-details/SellersDetailsController.js
+++ src/components/seller-details/SellersDetailsController.js
@@ -1,23 +1,38 @@
 "use strict";
 
 angular.module("project3App")
-.controller("SellersDetailsController",function SellersDetailsController($scope, $uibModal, AppResource,$routeParams,store, centrisNotify) {
+.controller("SellersDetailsController",function SellersDetailsController($scope, AppResource, $routeParams, store, centrisNotify, ProductDlg) {
 
   $scope.Sellerid = $routeParams.id ;
 
-  AppResource.getSellerDetails(parseInt($scope.Sellerid)).success(function (sellers){
+  AppResource.getSellerDetails(parseInt($scope.Sellerid)).success(function(sellers){
     $scope.sellersDetails = sellers;
-    console.log(sellers);
+  }).error(function() {
+    centrisNotify.error('sellers.Messages.LoadDetailsFailed');
   });
 
-  $scope.sellerID = $routeParams.id;
-  AppResource.getSellerProducts(parseInt($scope.sellerID)).success(function(products) {
-    $scope.sellersProducts = products;
-    console.log(products);
-    store.set('sellersProducts', $scope.sellersProducts);
-  }).error(function() {
-    centrisNotify.error('products.Messages.LoadFailed');
-  });
+  $scope.sellersProducts = store.get('sellersProducts');
+
+  $scope.onAddProduct = function onAddProduct() {
+      var p = {};
+      ProductDlg.show(p).then(function(product) {
+        AppResource.addSellerProduct(parseInt($scope.Sellerid), product).success(function(product) {
+          centrisNotify.success('product.Messages.SaveSucceeded');
+        }).error(function() {
+          centrisNotify.error('product.Messages.SaveFailed');
+      });
+    });
+  };
+
+
+    $scope.sellerID = $routeParams.id;
+    AppResource.getSellerProducts(parseInt($scope.sellerID)).success(function(products) {
+      $scope.sellersProducts = products;
+      console.log(products);
+      store.set('sellersProducts', $scope.sellersProducts);
+    }).error(function() {
+      centrisNotify.error('products.Messages.LoadFailed');
+    });
 
 
 }).directive('topProducts', function() {
