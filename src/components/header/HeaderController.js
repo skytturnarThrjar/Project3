b9309db8a15b4f7aa69566a3af0c6d612770e9ae
vala14 <vala14@ru.is>
2016-03-13 18:29:09 +0000

    back button

--- src/components/header/HeaderController.js
+++ src/components/header/HeaderController.js
@@ -1,12 +1,16 @@
 "use strict";
 
-angular.module("project3App").controller("HeaderController", ['$scope', '$translate', '$rootScope',
-function HeaderController($scope, $translate, $rootScope) {
+angular.module("project3App").controller("HeaderController", ['$scope', '$translate', '$rootScope', '$location',
+function HeaderController($scope, $translate, $rootScope, $location) {
 
     $rootScope.lang = 'en';
 
     $scope.changeLanguage = function(langKey) {
         $translate.use(langKey);
         $rootScope.lang = langKey;
+    };
+
+    $scope.goBack = function() {
+      $location.path('/');
     };
 }]);
