39229e32dacbad946c2717ca5b941bdf926f9b01
Laufey Rut Guðmundsdóttir <Laufey@Laufeys-MacBook-Pro.local>
2016-03-13 23:48:53 +0000

    laga div

--- src/components/product/ProductDirective.js
+++ src/components/product/ProductDirective.js
@@ -4,8 +4,5 @@ angular.module("project3App").directive('product', function() {
   return {
     restrict: 'E',
     templateUrl: '/src/components/product/Product.html',
-    link: function(scope, element) {
-      element.addClass('product');
-    }
   };
 });
